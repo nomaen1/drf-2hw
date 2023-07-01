d60ddea1f39abfa5746a69e04077423958cc464c
Janysh <tynybekovjanyshbek@gmail.com>
2023-07-01 16:03:36 +0600

    zaibal6

--- apps/setting/serializers.py
+++ apps/setting/serializers.py
@@ -7,4 +7,4 @@ from .models import HistoryTransfer
 class HistoryTransferSerializer(serializers.ModelSerializer):
     class Meta:
         model = HistoryTransfer
-        fields = ('id', 'from_user', 'to_user','is_completed', 'created_at', 'amount')
+        fields = ('id', 'from_user', 'to_user','is_completed', 'created_at', 'amount') 

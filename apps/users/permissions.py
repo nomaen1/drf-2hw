83bf4ab7baa4049957b664fac6811dc6d0595189
Janysh <tynybekovjanyshbek@gmail.com>
2023-07-01 19:24:39 +0600

    horosho8

--- apps/users/permissions.py
+++ apps/users/permissions.py
@@ -5,4 +5,4 @@ class UserPermission(BasePermission):
     def has_object_permission(self, request, view, obj):
         print(obj)
         print(request) 
-        return bool(obj.pk == request.user.pk)
+        return bool(obj.pk == request.user.pk)  

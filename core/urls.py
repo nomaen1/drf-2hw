2c842c4e37f20429d7aecd0b49222f0a950ce061
Janysh <tynybekovjanyshbek@gmail.com>
2023-07-01 19:22:40 +0600

    horosho3

--- core/urls.py
+++ core/urls.py
@@ -43,7 +43,7 @@ api_urlpatterns = [
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc')
 ]
-
+ 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(api_urlpatterns))
